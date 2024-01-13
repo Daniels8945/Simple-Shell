5663ef61ddec96b1507d7d8c1ea9f3aa3a763b5c
daniels8945 <obaj.daniels@gmail.com>
2024-01-12 16:18:57 -0800

    Header file for simple-shell

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef _SHELL_H__
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+#endif /* _SHELL_H_ */
